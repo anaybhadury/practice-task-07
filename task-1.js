28efcaa4d3cb3cbd890b1ffa1e868ccd47105a2f
anaybhadury <bhadury9292@gmail.com>
2024-09-07 20:03:07 +0600

    added practice task 1

---
+++ task-1.js
@@ -0,0 +1,9 @@
+/**
+ * TAKE FOUR PARAMETERS. MULTIPLY THE FOUR NUMBERS AND THEN RETURN THE RESULT
+ */
+
+function numbers(a, b, c, d){
+    return a * b * c * d;
+}
+const result = numbers(2, 2, 2, 2);
+console.log(result);
