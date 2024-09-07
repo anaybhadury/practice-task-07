948d8a3992c58d35d710407211b83718d1d06deb
anaybhadury <bhadury9292@gmail.com>
2024-09-07 20:29:51 +0600

    added practice task 2

---
+++ task-2.js
@@ -0,0 +1,37 @@
+/**
+ * TAKE A NUMBER IF THE NUMBER IS ODD MULTIPLY IT BY 2 AND RETURN THE RESULT. IF THE NUMBER IS EVEN DIVIDE IT BY TWO AND RETURN THE RESULT.
+ */
+
+// function checkNumbers3(num3){
+//     if(num3 % 2 !== 0){
+//         return num3 * 2;
+//     }
+//     else{
+//         return num3 / 2;
+//     }
+// }
+// console.log(checkNumbers3(5));
+// console.log(checkNumbers3(4));
+
+function checkNumbers2 (num2){
+    if(num2 % 2 === 0){
+        return num2 / 2;
+    }
+    else{
+        return num2 * 2;
+    }
+}
+console.log(checkNumbers2(5));
+console.log(checkNumbers2(4));
+
+// function checkNumbers(num){
+//     if(num % 2 === 0){
+//         return num / 2;
+//     }
+//     else{
+//         return num * 2;
+//     }
+// }
+// console.log(checkNumbers(5));
+// console.log(checkNumbers(19));
+// console.log(checkNumbers(4));
